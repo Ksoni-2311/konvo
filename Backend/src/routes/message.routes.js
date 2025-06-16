bb45296870df727e9da065a4dc70c1af061b6206
k2006 <k2006soni@gmail.com>
2025-06-16 22:59:27 +0530

    fix: adjust indentation for consistency in message routes

--- Backend/src/routes/message.routes.js
+++ Backend/src/routes/message.routes.js
@@ -1,10 +1,10 @@
-import express from 'express'
-import { protectRoute } from '../middleware/auth.middleware.js'
-import { getMessages, getUserForSidebar, sendMessage } from '../controllers/message.controllers.js'
-const router=express.Router()
+    import express from 'express'
+    import { protectRoute } from '../middleware/auth.middleware.js'
+    import { getMessages, getUserForSidebar, sendMessage } from '../controllers/message.controllers.js'
+    const router=express.Router()
 
-router.get('/users',protectRoute,getUserForSidebar)
-router.get('/:id',protectRoute,getMessages)
-router.post('/chat/:id',protectRoute,sendMessage)
+    router.get('/users',protectRoute,getUserForSidebar)
+    router.post('/chat/:id',protectRoute,sendMessage)
+    router.get('/:id',protectRoute,getMessages)
 
-export default router
+    export default router
