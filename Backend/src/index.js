e98eacee359102f10fa003b8056129832d92e036
k2006 <k2006soni@gmail.com>
2025-06-16 22:42:39 +0530

    fix: correct path method in sendFile for serving index.html

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -16,7 +16,7 @@ if(process.env.NODE_ENV==="production"){
 }
 
 app.get("*",(req,res)=>{
-    res.sendFile(path.json(__dirname,"../frontend","dist","index.html"))
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
 })
 
 const PORT=process.env.PORT;
