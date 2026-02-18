f4ecfa8cec5c3f712ea13b10edb6a9d657012725
Ksoni-2311 <k2006soni@gmail.com>
2026-02-18 19:17:55 +0530

    clean dependencies for render

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 console.log("cookieParser middleware added.");
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin:  process.env.CLIENT_URL,
   credentials: true
 }));
 console.log("CORS middleware added.");
@@ -51,22 +51,22 @@ try {
 }
 
 // Serve static files in production mode
-if (process.env.NODE_ENV === "production") {
-  const staticPath = path.join(__dirname, "../Frontend/dist");
-  console.log("Production mode detected. Serving static files from:", staticPath);
+// if (process.env.NODE_ENV === "production") {
+//   const staticPath = path.join(__dirname, "../Frontend/dist");
+//   console.log("Production mode detected. Serving static files from:", staticPath);
 
-  try {
-    app.use(express.static(staticPath));
-    console.log("Static middleware added.");
+//   try {
+//     app.use(express.static(staticPath));
+//     console.log("Static middleware added.");
 
-    app.get("*", (req, res) => {
-      res.sendFile(path.join(staticPath, "index.html"));
-    });
-    console.log("Catch-all route for SPA added.");
-  } catch (err) {
-    console.error("Error setting up static middleware or catch-all route:", err);
-  }
-}
+//     app.get("*", (req, res) => {
+//       res.sendFile(path.join(staticPath, "index.html"));
+//     });
+//     console.log("Catch-all route for SPA added.");
+//   } catch (err) {
+//     console.error("Error setting up static middleware or catch-all route:", err);
+//   }
+// }
 
 // Start server and connect DB
 server.listen(PORT, () => {
