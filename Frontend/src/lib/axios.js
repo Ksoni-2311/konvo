b1f0ded36ae58be4c4afd8ef2c19fd8a49994a7f
Ksoni-2311 <k2006soni@gmail.com>
2026-02-20 17:54:22 +0530

    fix: update axios instance baseURL for environment variables

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
-export const axiosInstance= axios.create({
-    baseURL:import.meta.env.MODE==="development"?"https://konvo-1.onrender.com/api":"/api",
-    withCredentials:true
-})
+export const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true
+});
 
 // - includes credentials: Setting withCredentials: true ensures that cookies and authentication data are sent with requests, useful for handling sessions.
--- Frontend/src/store/useAUthStore.js
+++ Frontend/src/store/useAUthStore.js
@@ -3,7 +3,8 @@ import { axiosInstance } from "../lib/axios.js"
 import toast from "react-hot-toast";
 import {io} from 'socket.io-client'
 
-const BASE_URL=import.meta.env.MODE==="development"?"https://konvo-1.onrender.com":"/"
+const BASE_URL = import.meta.env.VITE_SOCKET_URL; 
+
 export const userAuthStore=create((set,get)=>({
     authUser:null,
     isSignedUp:false,
